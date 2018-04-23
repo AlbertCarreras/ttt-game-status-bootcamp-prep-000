2a0dce6e766d06b2f2510199a14cac686cbaa0f7
Alberto Carreras <acarrerasc@gmail.com>
2018-04-23 04:16:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -55,9 +55,9 @@ def draw?(board)
  end
     
 def over?(board)
-  status = ""
+  status = false
   if won?(board) != false || draw?(board) == true || full?(board) == true
-    status = true
+      status = true
   end
   status
 end

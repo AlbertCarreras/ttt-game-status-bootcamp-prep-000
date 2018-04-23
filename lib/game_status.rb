d465cc18d6c676e92f9d7f9eb836d7daaa8bcb8c
Alberto Carreras <acarrerasc@gmail.com>
2018-04-23 02:59:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,5 +18,8 @@ def won(board)
   WIN_COMBINATIONS.each do |win_combination|
 
       if board[win_combination][0] == "X" && board[win_combination][1] == "X" && board[win_combination][2] == "X" 
-
+        "X won in the top row"
+        win_combination
+      elsif board[win_combination][0] == "O" && board[win_combination][1] == "O" && board[win_combination][2] == ")" 
+        "O won in the top row"
 end

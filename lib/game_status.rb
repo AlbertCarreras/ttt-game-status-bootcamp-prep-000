8c9d22fd2a4fb12bc8e94343b5d55b61d57b3ea5
Alberto Carreras <acarrerasc@gmail.com>
2018-04-23 03:59:03 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -44,6 +44,9 @@ end
   
 def draw?(board)
   if !won?(board) && full(board)
-    elsif won?(board) && full(board)
+      true
+    elsif !won?(board) && !full(board)
+      false
+    elsif
     
     

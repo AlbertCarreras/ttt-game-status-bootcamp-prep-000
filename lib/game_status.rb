d182ba03a08937ae786ab7fd28d4cb040f574bbb
Alberto Carreras <acarrerasc@gmail.com>
2018-04-23 03:40:29 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,9 +16,9 @@ WIN_COMBINATIONS = [
   
 def won?(board) 
  if WIN_COMBINATIONS.each do |win_combination|
-      win_index_1 = win_combination[0].to_i
-      win_index_2 = win_combination[1].to_i
-      win_index_3 = win_combination[2].to_i
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
       position_1 = board[win_index_1]
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]

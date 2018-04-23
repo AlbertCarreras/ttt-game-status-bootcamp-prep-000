fd14a52d29847a40eebf059c19fa658dbd1291e4
Alberto Carreras <acarrerasc@gmail.com>
2018-04-23 03:29:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,7 @@ def won?(board)
       elsif board[position_1] == "O" && board[position_2] == "O" && board[position_3] == "O"
         win_combination
       else 
-        false
+
     end
     
   end
